4f13c64cb117a9315c315a9f1f67b909b6b005ff
Tiago <tiagobassini@gmail.com>
2022-03-14 10:47:02 -0300

    Incluido um botao para play/pause da musica.

--- src/js/sound_script.js
+++ src/js/sound_script.js
@@ -1,21 +1,27 @@
+const playPauseCheck = document.getElementById("playpause-check")
+let isPlaying = false
+let audio = new Audio("src/som/01 Opening (part 1).mp3");
+audio.volume = 0.02;
+audio.addEventListener("ended", () => {
+    nextMusic();
+}, true);
 
 
+playPauseCheck.addEventListener("click", () => {
+    if (isPlaying) {
+        isPlaying = false
+        audio.pause()
+    } else {
+        isPlaying = true
+        audio.play()
+    }
+})
 
-
-(function () {
-    let audio = new Audio("src/som/01 Opening (part 1).mp3");
-    audio.volume = 0.02;
-    audio.play();
-    audio.addEventListener("ended", function () {
-        nextMusic();
-    }, true);
-})();
 
 function nextMusic() {
 
-    let audio = new Audio("src/som/02 Opening (part 2).mp3");
+    audio = new Audio("src/som/02 Opening (part 2).mp3");
     audio.volume = 0.02;
     audio.loop = true;
     audio.play();
 }
-
